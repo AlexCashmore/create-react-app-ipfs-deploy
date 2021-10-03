72dfab39c38363cf6f72fd70b6d27b3491e93d1a
Alex Cashmore <alex@aider.ai>
2021-10-03 22:21:39 +1300

    copy

--- src/CV.js
+++ src/CV.js
@@ -39,11 +39,11 @@ export const CV = ()=>{
 
             <h4>Aider International.      March 2021 – Present</h4>
             <p style={{width:650,textAlign:'left'}}>
-                Role: Lead Software Engineer
-                Responsibilities
+                <p>Role: Lead Software Engineer</p>
+                Responsibilities <br />
                 Design, develop and maintain a brand new platform for Accounting compliance, leveraging a powerful artificial intelligence backend platform. Implementing best practices and guiding refactoring of legacy code to best suit an application built to scale.
                 Assisting in product planning and development, engaged in both UI/UX design and feature architecture.
-                Key Achievements
+                <br/><br />Key Achievements<br />
                 Designed and Launched the Aider Advisory Portal platform, consisting of a website a powerful Typescript Node/Python NLU backend , OAuth2, GraphQL with Hasura, and Firestore database hosted on Google’s cloud platform.
                 Assisted in product onboardings of Tier 1 Accounting Firms.
                 Maintenance of assisting mobile app during gaps of mobile resource.
@@ -54,12 +54,12 @@ export const CV = ()=>{
             <h4>Coretex.     Nov 2018 – March 2021</h4>
             <p style={{width:650,textAlign:'left'}}>
                 A Cold Chain and Transport Compliance Solution Provider, delivering varying solutions to medium and large enterprises based in the United States.
-                Role: Lead Software Engineer
-                Responsibilities
+                <p>Role: Lead Software Engineer</p>
+                Responsibilities <br />
                 Create from scratch a brand new platform, implementing best practices with a holistic approach to software engineering.
                 Turning high level business concepts and pain points into real features and solutions.
                 Leading sprint planning, backlog refinement, task estimation, dissecting project requirements into small implementable tasks and personally delivering value to the customers.
-                Key Achievements
+                <br/><br/>Key Achievements<br/>
                 Designed and Created the Core Temp compliance platform, consisting of a website and database, management service and telematics integration processor.
                 Delivered integration of the product to 37 Sites over 9 Organizations
                 Assisted the hiring and building of a team of passionate staff to assist with the many roles that make a software team successful with a combination of contractors and permanents that work in harmony.
@@ -70,13 +70,14 @@ export const CV = ()=>{
 
             <h4>Archoneer.      Aug 2017 – October 2018</h4>
             <p style={{width:650,textAlign:'left'}}>
-                A highly functional and professional Startup based in Albany focused on delivering innovative solutions in a variety of verticals such as Pharmaceuticals, Medicine and Education  Role: Software Engineer
-                Responsibilities
+                A highly functional and professional Startup based in Albany focused on delivering innovative solutions in a variety of verticals such as Pharmaceuticals, Medicine and Education  <br/>
+                <p>Role: Software Engineer</p>
+                Responsibilities <br />
                 Lead Fullstack developer of Health Software and Website based Administration systems for Pharmaceutical Enterprises. Involvement of UI design and integration testing.
                 Versatility on projects that required time sensitive Frontend and Backend development as well as important DevOps tasks.
                 Writing both Client specification and Technical documentation for inhouse development.
                 Assisting with the implementation of Agile and Scrum methodology.
-                Key Achievements
+                <br/><br/>Key Achievements<br/>
                 Designed and Created the Zoom Health Pharmacy platform, consisting of a pharmaceutical administration system, customer facing mobile app and software to integrate with Doctors and GPs workflow
                 Delivered the platform to a User base of over 1000 active daily users between iOS and Android
                 On boarded the expert system with 9 Pharmacists and over 100 active GPs
